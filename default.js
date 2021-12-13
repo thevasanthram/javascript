1dbdfa53fb5ee4211577f7a31530018fc019ca04
thevasanthram <82960398+thevasanthram@users.noreply.github.com>
2021-12-13 20:20:48 +0530

    first_change

---
+++ default.js
@@ -0,0 +1,9 @@
+document.write("<h1>Welcome and Warm greetings</h1>");
+
+var cars=["benz","Audi","BMW"];
+cars.push("Nexon");
+cars.pop();
+for(i=0;i<cars.length;i++)
+{
+    document.write(cars[i] + " ");
+}
